c6e5db8ec03141269016e00798d5c4656db0e8f7
David Sword <david.sword@automattic.com>
2020-05-26 08:29:18 -0700

    phpbf

--- colour-picker.php
+++ colour-picker.php
@@ -14,8 +14,8 @@ $workflow = new Workflows();
 
 $query = ! isset( $argv[1] ) || empty( $argv[1] ) ? false : $argv[1];
 
-$query      = trim( $query );
-$subtitle   = '';
+$query    = trim( $query );
+$subtitle = '';
 
 $colours = json_decode( file_get_contents( 'colour-lib.json' ), JSON_OBJECT_AS_ARRAY );
 foreach ( $colours as $colour ) {
@@ -27,16 +27,18 @@ foreach ( $colours as $colour ) {
 	if ( ! $query || ( $query && search_keyword_in_string( $query, $name ) ) ) {
 
 		$text_name = trim( preg_replace( '/[^a-z0-9_\-]/', '', $name ) );
-		$icon = file_exists("images/icons/{$text_name}.png") ? "images/icons/{$text_name}.png" : '';
+		$icon      = file_exists( "images/icons/{$text_name}.png" ) ? "images/icons/{$text_name}.png" : '';
 
 		$workflow->result(
 			md5( $hex ),
-			json_encode([
-				'name' => $name,
-				'text_name' => $text_name,
-				'rgb'  => $rgb,
-				'hex'  => $hex,
-			]),
+			json_encode(
+				[
+					'name'      => $name,
+					'text_name' => $text_name,
+					'rgb'       => $rgb,
+					'hex'       => $hex,
+				]
+			),
 			ucfirst( $text_name ),
 			$subtitle,
 			$icon
